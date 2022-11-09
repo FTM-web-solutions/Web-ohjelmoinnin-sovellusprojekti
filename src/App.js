19fe7b902a73dd8724376bfea23e0b3191303571
Joona Hautaniemi <t1hajo02@students.oamk.fi>
2022-11-09 14:39:43 +0200

    SPA alkuversio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
+import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
 
 const URL = 'http://localhost:3001/'
 
@@ -18,13 +26,24 @@ function App() {
   }, [])
 
   return (
-    <div>
+    <>
+    <Navbar />
+    <Header />
+    <div className='container'>
       <h1>FTM web proujekt</h1>
       <ol>{crut.map(hadcrut=>(
           <li key={hadcrut.Years}>{hadcrut.Years}{hadcrut.AnnualGlobalC}{hadcrut.AnnualNorthC}{hadcrut.AnnualSouthC}</li>
         ))}
       </ol>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
+    <Footer />
+    </>
   );
 }
 
