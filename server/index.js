7c6df447144b23c8ef1e9debd4d974a5163bda90
LeeviJarvinen <t0jale00@students.oamk.fi>
2022-11-15 15:00:31 +0200

    v5 alustus

--- server/index.js
+++ server/index.js
@@ -11,10 +11,24 @@ app.use(express.urlencoded({extended: false}))
 
 const port = 3001
 
+//GET Visualizations here
+
 app.get("/",async function (req,res)    {
     try {
         const connection = await mysql.createConnection(config.db)
         const[result,] = await connection.execute('select * from hadcrut')
+        
+        if (!result) result=[]
+        res.status(200).json(result)
+    }   catch(err) {
+        res.status(500).json({error: err.message})
+    }
+})
+
+app.get("/v5",async function (req,res)    {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        const[result,] = await connection.execute('select * from v5')
         
         if (!result) result=[]
         res.status(200).json(result)
