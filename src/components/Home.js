185532be560b76387a4126e8a483406ba5a71211
LeeviJarvinen <t0jale00@students.oamk.fi>
2022-11-16 14:40:10 +0200

    V6 viiva chartti tehty

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import V1 from './V1'
 import V5 from './V5'
+import V6 from './V6'
 
 export default function Home() {
   return (
@@ -8,6 +9,8 @@ export default function Home() {
         <V1/>
         
         <V5/>
+
+        <V6/>
         
     </div>
   )
