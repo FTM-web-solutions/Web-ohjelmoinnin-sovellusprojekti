0fa0c8e94bf465693dd8ac527e701614cf88761c
LeeviJarvinen <t0jale00@students.oamk.fi>
2022-12-09 16:20:59 +0200

    kona gorner urut laulaa

--- src/components/N3.js
+++ src/components/N3.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import V1 from "./V1"
 import V3 from "./V3"
 import V5 from "./V5"
@@ -90,84 +91,29 @@ function N3() {
     
   return (
     <>
-    <div>
-          <h1 className="form-head-contact-h3 ">
-            Customise your own visualization!
-          </h1>
-		  <p>Here you can select your preferred choices from every visualization</p>
-          <input
-          type="checkbox"
-          id="v1"
-          name="v1"
-          value="v1"
-          checked={v1state}
-          onChange={v1handler}
-          />
-          V1
-		  <br />
-          <input
-          type="checkbox"
-          id="v3"
-          name="v3"
-          value="v3"
-          checked={v3state}
-          onChange={v3handler}
-          />
-          V3
-		  <br />
-          <input
-          type="checkbox"
-          id="v5"
-          name="v5"
-          value="v5"
-          checked={v5state}
-          onChange={v5handler}
-          />
-          V5
-		  <br />
-          <input
-          type="checkbox"
-          id="v6"
-          name="v6"
-          value="v6"
-          checked={v6state}
-          onChange={v6handler}
-          />
-          V6
-		  <br />
-          <input
-          type="checkbox"
-          id="v7"
-          name="v7"
-          value="v7"
-          checked={v7state}
-          onChange={v7handler}
-          />
-          V7
-		  <br />
-          <input
-          type="checkbox"
-          id="v8"
-          name="v8"
-          value="v8"
-          checked={v8state}
-          onChange={v8handler}
-          />
-          V8
-		  <br />
-          <input
-          type="checkbox"
-          id="v9"
-          name="v9"
-          value="v9"
-          checked={v9state}
-          onChange={v9handler}
-          />
-          V9
-          </div>
-          <div>
-            {v1}{v3}{v5}{v6}{v7}{v8}{v9}
-          </div>
+      <div>
+        <h1 className="form-head-contact-h3 ">
+          Customise your own visualization!
+        </h1>
+        <p>Here you can select your preferred choices from every visualization</p>
+        <ul className="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Visualizations</a>
+            <ul class="dropdown-menu">
+              <li><input type="checkbox" id="v1" name="v1" value="v1" checked={v1state} onChange={v1handler} />Hadcrut temp data</li>
+              <li><input type="checkbox" id="v3" name="v3" value="v3" checked={v3state} onChange={v3handler} />Atmospheric Co2 data</li>
+              <li><input type="checkbox" id="v5" name="v5" value="v5" checked={v5state} onChange={v5handler} />Vostok Co2 data</li>
+              <li><input type="checkbox" id="v6" name="v6" value="v6" checked={v6state} onChange={v6handler} />Ice core composite data</li>
+              <li><input type="checkbox" id="v7" name="v7" value="v7" checked={v7state} onChange={v7handler} />Global temp data</li>
+              <li><input type="checkbox" id="v8" name="v8" value="v8" checked={v8state} onChange={v8handler} />Co2 emission by country</li>
+              <li><input type="checkbox" id="v9" name="v9" value="v9" checked={v9state} onChange={v9handler} />Co2 emission by sector</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div>
+        {v1}{v3}{v5}{v6}{v7}{v8}{v9}
+      </div>
     </>
   );
 }
